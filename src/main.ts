de337e917335899e49719168ade45c44e84f2451
menger <1092648062@qq.com>
2025-02-07 17:25:10 +0800

    feat： elementplus的基础配置&默认主题更改 默认主题还有部分组件未生效的问题（未能覆盖原样式），但是我想去吃饭了

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,30 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createRouter, createWebHistory } from "vue-router";
 
-createApp(App).mount('#app')
+// import "@/styles/element/index.scss";
+
+import ElementPlus from "element-plus";
+// import all element css, uncommented next line
+import "element-plus/dist/index.css";
+
+import { routes } from "vue-router/auto-routes";
+
+import App from "./App.vue";
+import "@/styles/index.scss";
+
+
+import "element-plus/theme-chalk/src/message.scss";
+import "element-plus/theme-chalk/src/message-box.scss";
+
+// createApp(App).mount('#app')
+
+import { createApp } from "vue";
+
+const app = createApp(App);
+app.use(
+  createRouter({
+    history: createWebHistory(),
+    routes,
+  })
+);
+app.use(ElementPlus);
+app.mount("#app");
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,43 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import VueRouter from "unplugin-vue-router/vite";
+import Components from "unplugin-vue-components/vite";
 
+
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
   resolve: {
     alias: {
       "@": "/src/",
     },
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/styles/element/index.scss" as *;`,
+        api: "modern-compiler",
+      },
+    },
+  },
+  plugins: [
+    vue(),
+
+    VueRouter({
+      extensions: [".vue", ".md"],
+      dts: "src/typed-router.d.ts",
+    }),
+    Components({
+      // allow auto load markdown components under `./src/components/`
+      extensions: ["vue", "md"],
+      // allow auto import and register components used in markdown
+      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+      resolvers: [
+        ElementPlusResolver({
+          importStyle: "sass",
+        }),
+      ],
+      dts: "src/components.d.ts",
+    }),
+  ],
 });
